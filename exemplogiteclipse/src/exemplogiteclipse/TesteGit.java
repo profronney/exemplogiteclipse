cfc4688aeda99023b8b69f77a7048f5c1e42e9b8
ronney <ronney@localhost>
2021-06-07 21:15:34 -0300

    Alteração no Eclipse

--- exemplogiteclipse/src/exemplogiteclipse/TesteGit.java
+++ exemplogiteclipse/src/exemplogiteclipse/TesteGit.java
@@ -5,6 +5,7 @@ public class TesteGit {
 	public static void main(String[] args) {
 		System.out.println("Teste de Git");
 		System.out.println("Alteração no Git");
+		System.out.println("Alteração no Eclipse");
 	}
 
 }
